b6f8995990462e8e74406797e57ed89342df6a52
Ludmila Martos <95381011+Ludmimar@users.noreply.github.com>
2025-07-16 18:31:54 -0300

    Delete Mod06 - Proyecto Integrador Etapa 1 directory

--- Mod06 - Proyecto Integrador Etapa 1/models/Movimiento.js
+++
@@ -1,7 +0,0 @@
-class Movimiento {
-  constructor(tipo, monto) {
-    this.tipo = tipo;
-    this.monto = parseFloat(monto);
-    this.fecha = new Date(); // La fecha se convierte en string al serializar
-  }
-}
